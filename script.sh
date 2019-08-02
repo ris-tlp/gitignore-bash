96a07a7aa00dca046374a0a781384a94a1ddbd5d
Omar <omarkhantlp@gmail.com>
2019-08-02 15:52:45 +0300

    added checks

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
-echo "HELLO"
+#!/bin/bash
+
+PATH=./.gitignore
+[ ! -f $PATH ] && eval touch $PATH
+
+if eval grep -R "$1" "$PATH"
+then
+	eval echo "File already exists in gitignore."
+else
+	eval cat "$1" >> "$PATH"
+fi
+
